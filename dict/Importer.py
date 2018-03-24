d93062191a837a6e0da0c29aac4b08fc13bfc456
Henrik Drefs <henrik.drefs@volkswagen.de>
2018-03-24 15:13:56 +0100

    added buffering

--- dict/Importer.py
+++ dict/Importer.py
@@ -7,6 +7,8 @@ from dict.DictEntry import DictEntry
 
 
 class Importer:
+    DEFAULT_BUFFER_SIZE = 1024
+
     def __init__(self, host: str = 'localhost', port: int = 27017):
         self.client = MongoClient(host, port)
         self.db = self.client.dict
@@ -15,11 +17,15 @@ class Importer:
         result = self.db.deen.delete_many({})
         return result.deleted_count
 
-    def import_all(self, dict_entries: Generator[DictEntry, Any, None]):
+    def import_all(self, dict_entries: Generator[DictEntry, Any, None], buffer_size: int = DEFAULT_BUFFER_SIZE):
+        # todo check buffer size
+        buffer = []
         for dict_entry in dict_entries:
-            self.db.deen.insert_one(dict_entry.__dict__)
+            buffer.append(dict_entry)
+            if len(buffer) >= buffer_size:
+                self.db.deen.insert_many(buffer)
+                buffer = []
+        self.db.deen.insert_many(buffer)
 
     def import_entry(self, dict_entry: DictEntry):
-        # if dict_entry.gender:
-        #     dict_entry.gender = dict_entry.gender.name
         self.db.deen.insert_one(dict_entry.__dict__)
